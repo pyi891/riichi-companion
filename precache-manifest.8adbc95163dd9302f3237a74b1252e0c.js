554eb360a492f711743f23153d81aed96a3155dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 03:41:23 +0000

    Deploy pyi891/riichi-companion to github.com/pyi891/riichi-companion.git:gh-pages

---
+++ precache-manifest.8adbc95163dd9302f3237a74b1252e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac51ac656f9216c5615ead7c5d65c134",
+    "url": "/riichi-companion/index.html"
+  },
+  {
+    "revision": "85d14f8c146e911ada02",
+    "url": "/riichi-companion/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d7f722bc088be23c1312",
+    "url": "/riichi-companion/static/css/main.3f960433.chunk.css"
+  },
+  {
+    "revision": "85d14f8c146e911ada02",
+    "url": "/riichi-companion/static/js/2.82251502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/riichi-companion/static/js/2.82251502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f722bc088be23c1312",
+    "url": "/riichi-companion/static/js/main.bd83b0af.chunk.js"
+  },
+  {
+    "revision": "87e6368501332e67356d",
+    "url": "/riichi-companion/static/js/runtime-main.85c8ad82.js"
+  }
+]);
